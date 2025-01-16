476ced64048d79e317176355ea1f9f0ae783acd9
DJ McKay <djmckay@mac.com>
2025-01-16 13:11:18 -0500

    placeholder for visitor counter

--- Public/scripts/cookies.js
+++ Public/scripts/cookies.js
@@ -7,3 +7,21 @@ var expires = "expires="+ d.toUTCString();
 document.cookie = "cookies-accepted=true;" + expires;
 }
 
+function fetchData() {
+
+  fetch("https://tba.execute-api.eu-west-2.amazonaws.com/prod/", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => takeData(json));
+}
+
+function takeData(val) {
+
+  document.getElementById('visitor').innerHTML = val.Attributes.numberOfVisitors;
+}
+
+//fetchData();
